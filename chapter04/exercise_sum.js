bae22d435597e7ecd33c439aea4adf86b8922562
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-02 11:46:50 +0900

    add static server1

--- chapter04/exercise_sum.js
+++ chapter04/exercise_sum.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const url = require("url");
 var querystring = require("querystring");
-//#region
+const { query } = require("express");
+//#region get방식
 var server = http
   .createServer(function (req, res) {
     if (req.method.toLowerCase() === "get") {
@@ -24,6 +25,42 @@ var server = http
   .listen(8080, function () {
     console.log("8080 포트에서 대기중");
   });
+//#endregion
+
+//#region 2. post 방식으로 넘어온 두 값(start, end)을 활용하여 두 값 사이의 합계 구하기
+var fs = require("fs");
+
+var server = http
+  .createServer((req, res) => {
+    if (req.method.toLowerCase() === "post") {
+      var body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+      req.on("end", () => {
+        var data = querystring.parse(body); // num1=1&num2=100 => {'num1':1, 'num2':2}
+        var num1 = parseInt(data.num1);
+        var num2 = parseInt(data.num2);
+
+        res.setHeader("Content-Type", "text/html; charset=utf-8");
+        if (Number.isNaN(num1) || Number.isNaN(num2)) {
+          res.end("잘못된 숫자가 입력되어 에러가 발생함.");
+        } else {
+          var sum = 0;
+          for (var i = num1; i <= num2; i++) sum += i;
+          res.end(`${num1} ~ ${num2}의 합계: ${sum}`);
+        }
+      });
+    } else {
+      if (req.url === "/") {
+        fs.readFile("./sum.html", (err, data) => {
+          res.end(data);
+          return;
+        });
+      }
+    }
+  })
+  .listen(8080, () => console.log("8080 포트에서 대기중"));
 //#endregion
 
 function showForm(res) {
